7cc47b07da8c6e3437458b6a321c080c9ac2c52b
Oliver Sõro <oliver.s6ro@gmail.com>
2019-09-08 13:53:30 +0300

    First read through, understand function signatures.

--- FablExplr/FablExplr.fs
+++ FablExplr/FablExplr.fs
@@ -22,14 +22,14 @@ module App =
 
     let initModel = { Count = 0; Step = 1; TimerOn=false }
 
-    let init () = initModel, Cmd.none
+    let init () : Model * Cmd<'a> = initModel, Cmd.none
 
-    let timerCmd =
+    let timerCmd : Cmd<Msg> =
         async { do! Async.Sleep 200
                 return TimedTick }
         |> Cmd.ofAsyncMsg
 
-    let update msg model =
+    let update (msg : Msg) (model : Model) : Model * Cmd<Msg> =
         match msg with
         | Increment -> { model with Count = model.Count + model.Step }, Cmd.none
         | Decrement -> { model with Count = model.Count - model.Step }, Cmd.none
@@ -42,18 +42,51 @@ module App =
             else 
                 model, Cmd.none
 
-    let view (model: Model) dispatch =
+    let view (model: Model) (dispatch : Dispatch<Msg>) : ViewElement =
         View.ContentPage(
           content = View.StackLayout(padding = 20.0, verticalOptions = LayoutOptions.Center,
             children = [ 
-                View.Label(text = sprintf "%d" model.Count, horizontalOptions = LayoutOptions.Center, widthRequest=200.0, horizontalTextAlignment=TextAlignment.Center)
-                View.Button(text = "Increment", command = (fun () -> dispatch Increment), horizontalOptions = LayoutOptions.Center)
-                View.Button(text = "Decrement", command = (fun () -> dispatch Decrement), horizontalOptions = LayoutOptions.Center)
-                View.Label(text = "Timer", horizontalOptions = LayoutOptions.Center)
-                View.Switch(isToggled = model.TimerOn, toggled = (fun on -> dispatch (TimerToggled on.Value)), horizontalOptions = LayoutOptions.Center)
-                View.Slider(minimumMaximum = (0.0, 10.0), value = double model.Step, valueChanged = (fun args -> dispatch (SetStep (int (args.NewValue + 0.5)))), horizontalOptions = LayoutOptions.FillAndExpand)
-                View.Label(text = sprintf "Step size: %d" model.Step, horizontalOptions = LayoutOptions.Center) 
-                View.Button(text = "Reset", horizontalOptions = LayoutOptions.Center, command = (fun () -> dispatch Reset), canExecute = (model <> initModel))
+                View.Label
+                    ( text = sprintf "%d" model.Count
+                    , horizontalOptions = LayoutOptions.Center
+                    , widthRequest=200.0
+                    , horizontalTextAlignment=TextAlignment.Center
+                    )
+                View.Button
+                    ( text = "Increment"
+                    , command = (fun () -> dispatch Increment)
+                    , horizontalOptions = LayoutOptions.Center
+                    )
+                View.Button
+                    ( text = "Decrement"
+                    , command = (fun () -> dispatch Decrement)
+                    , horizontalOptions = LayoutOptions.Center
+                    )
+                View.Label
+                    ( text = "Timer"
+                    , horizontalOptions = LayoutOptions.Center
+                    )
+                View.Switch
+                    ( isToggled = model.TimerOn
+                    , toggled = (fun on -> dispatch (TimerToggled on.Value))
+                    , horizontalOptions = LayoutOptions.Center
+                    )
+                View.Slider
+                    ( minimumMaximum = (0.0, 10.0)
+                    , value = double model.Step
+                    , valueChanged = (fun args -> dispatch (SetStep (int (args.NewValue + 0.5))))
+                    , horizontalOptions = LayoutOptions.FillAndExpand
+                    )
+                View.Label
+                    ( text = sprintf "Step size: %d" model.Step
+                    , horizontalOptions = LayoutOptions.Center
+                    ) 
+                View.Button
+                    (text = "Reset"
+                    , horizontalOptions = LayoutOptions.Center
+                    , command = (fun () -> dispatch Reset)
+                    , canExecute = (model <> initModel)
+                    )
             ]))
 
     // Note, this declaration is needed if you enable LiveUpdate
